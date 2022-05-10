7c32aa1c187b4c0dcd473a657dedb1473dec9137
Mergea Sergiu <mergeasergiu@gmail.com>
2022-05-10 19:41:12 +0300

    TASK-SPOR-15

--- src/main/java/com/example/tema1/CustomerViewController.java
+++ src/main/java/com/example/tema1/CustomerViewController.java
@@ -1,5 +1,6 @@
 package com.example.tema1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -29,11 +30,42 @@ public class CustomerViewController {
     @FXML
     private Button teren_tenis;
 
-    public void action_button_home() throws IOException {
+    @FXML
+    void action_button_basketball_court(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_button_handbal_court(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_button_home(ActionEvent event) throws IOException {
         Main l = new Main();
         l.changeScene("FirstPage.fxml");
     }
 
+    @FXML
+    void action_button_inside_footbal_court(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_button_outside_football_court(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_button_tenis_court(ActionEvent event) {
+
+    }
+
 }
+
+
+
+
+
 
 
