ada28a5e5a7081328495694979ea076ae9f62c05
Mergea Sergiu <mergeasergiu@gmail.com>
2022-05-16 00:57:59 +0300

    TASK SPOR-16

--- src/main/java/com/example/tema1/TerenHandbalCustomerView.java
+++ src/main/java/com/example/tema1/TerenHandbalCustomerView.java
@@ -1,4 +1,67 @@
 package com.example.tema1;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class TerenHandbalCustomerView {
+
+    @FXML
+    private MenuItem Ora5_6;
+
+    @FXML
+    private MenuItem Ora6_7;
+
+    @FXML
+    private MenuItem Ora7_8;
+
+    @FXML
+    private MenuItem Ora8_9;
+
+    @FXML
+    private MenuItem Ora9_10;
+
+    @FXML
+    private ImageView back_image;
+
+    @FXML
+    private Button button_home_page;
+
+    @FXML
+    private Button button_menu_page;
+
+    @FXML
+    private DatePicker calendar_data;
+
+    @FXML
+    private CheckBox check_caldura;
+
+    @FXML
+    private ImageView home_image;
+
+    @FXML
+    private Label label_interval_orar;
+
+    @FXML
+    private Label label_name;
+
+    @FXML
+    private Label label_numepagina;
+
+    @FXML
+    private Label label_selecteaza_data;
+
+    @FXML
+    private MenuButton menu_button;
+
+    @FXML
+    private TextField text_username;
+
 }
+
