ef7eb4c86d928f651fb43f3f6708b5f88ae5abdc
Mergea Sergiu <mergeasergiu@gmail.com>
2022-05-17 23:28:59 +0300

    SPOR-55 Am adaugat get-erele si set-erele pentru clasa Reservations

--- src/main/java/com/example/tema1/Reservations.java
+++ src/main/java/com/example/tema1/Reservations.java
@@ -21,4 +21,53 @@ public class Reservations {
      this.Court = Court;
  }
 
+ public void setIDReservation(int IDReservation){
+     this.IDReservation=IDReservation;
+ }
+
+ public int getIDReservation(){
+     return this.IDReservation;
+ }
+
+ public void setName(String Name){
+     this.Name = Name;
+ }
+
+ public String getName(){
+     return this.Name;
+ }
+
+ public void setDate(Date Date){
+     this.Date = Date;
+ }
+
+ public Date getDate(){
+     return this.Date;
+ }
+
+ public void setTime_Schedule(String Time_Schedule){
+     this.Time_Schedule=Time_Schedule;
+ }
+
+ public String getTime_Schedule(){
+     return this.Time_Schedule;
+ }
+
+ public void setHeat(boolean Heat){
+     this.Heat = Heat;
+ }
+
+ public boolean getHeat(){
+     return this.Heat;
+ }
+
+ public void setCourt(String Court){
+     this.Court = Court;
+ }
+
+ public String getCourt(){
+     return this.Court;
+ }
+
+
 }
