6f1107b28f323315608095e0908e374063f953e7
Mergea Sergiu <mergeasergiu@gmail.com>
2022-05-19 20:35:26 +0300

    SPOR-65 Am creat butoanele de go to home_page, login_page, sign_up page

--- src/main/java/com/example/tema1/AboutUsController.java
+++ src/main/java/com/example/tema1/AboutUsController.java
@@ -1,5 +1,71 @@
 package com.example.tema1;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class AboutUsController {
+
+    @FXML
+    private Button home_page_button;
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private Label label2;
+
+    @FXML
+    private Label label3;
+
+    @FXML
+    private Label label4;
+
+    @FXML
+    private Label label5;
+
+    @FXML
+    private Label label_numele_noastre;
+
+    @FXML
+    private Button log_in_button;
+
+    @FXML
+    private Label login_label;
+
+    @FXML
+    private ImageView menu_image;
+
+    @FXML
+    private Label nume_centru;
+
+    @FXML
+    private ImageView poza1;
+
+    @FXML
+    private ImageView poza2;
+
+    @FXML
+    private Button sign_up_button;
+
+    @FXML
+    private Label sign_up_label;
+
+    @FXML
+    void home_page_action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void log_in_page(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sign_up_page(ActionEvent event) {
+
+    }
 
 }
